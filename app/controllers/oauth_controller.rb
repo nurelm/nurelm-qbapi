c7b38cd2243c8988d77e701d5f454ec4c595caa7
Sam Shaaban <sam@nurelm.com>
2015-12-10 21:00:30 -0500

    Dump a page of customers

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -18,11 +18,21 @@ class OauthController < ActionController::Base
       token = access_token.token
       secret = access_token.secret
       realm_id = params['realmId']
-      # store the token, secret & RealmID somewhere for this user, you will need all 3 to work with Quickbooks-Ruby
 
+      # store the token, secret & RealmID somewhere for this user, you will need all 3 to work with Quickbooks-Ruby
       @token = token
       @secret = secret
       @realm_id = realm_id
+
+      ## Normally done elsewhere, when you start to do stuff
+      access_token = OAuth::AccessToken.new($qb_oauth_consumer, @token, @secret)
+      service = Quickbooks::Service::Customer.new
+      service.company_id = @realm_id
+      service.access_token = access_token # the OAuth Access Token you have from above
+
+      # Equivalent to Quickbooks::Service::Customer.new(:company_id => "123", :access_token => access_token)
+
+      @customers = service.query() # Called without args you get the first page of results
   end
 
 end
